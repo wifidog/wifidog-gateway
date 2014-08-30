53a7c05244edda0c20cb6703e5028d89f727b87d
Kontinuation <morisama@yeah.net>
2014-08-30 11:29:54 +0800

    fixed boundary condition when line terminator was not found

--- src/conf.c
+++ src/conf.c
@@ -265,7 +265,7 @@ parse_auth_server(FILE *file, const char *filename, int *linenum)
 		}
 
 		/* trim all blanks at the end of the line */
-		for (p2 = (p2 != NULL? p2 - 1: &line[MAX_BUF - 1]);
+		for (p2 = (p2 != NULL? p2 - 1: &line[MAX_BUF - 2]);
 		     isblank(*p2) && p2 > p1; p2--) {
 			*p2 = '\0';
 		}
