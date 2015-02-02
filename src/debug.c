ee9948fbcee8b554134c88d28fa6642fe8d8a4a7
Lilian <florida@assf.fr>
2015-02-02 14:43:40 +0100

    fix "Avoid deadlocks when the LOG_LEVEL is set to DEBUG" commit
    see #41
    
    thank sfgxuan

--- src/debug.c
+++ src/debug.c
@@ -75,8 +75,8 @@ _debug(const char *filename, int line, int level, const char *format, ...)
             vsyslog(level, format, vlist);
             va_end(vlist);
             closelog();
-
-            sigprocmask(SIG_UNBLOCK, &block_chld, NULL);
 	}
+        
+        sigprocmask(SIG_UNBLOCK, &block_chld, NULL);
     }
 }
