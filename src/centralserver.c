46f33964e6646bf69f807735484d170e1cc486de
Michael Haas <haas@computerlinguist.org>
2015-03-08 10:11:04 +0100

    Remove Unreachable Code Warning
    Returning AUTH_ERROR is a reasonable thing to do

--- src/centralserver.c
+++ src/centralserver.c
@@ -172,11 +172,7 @@ auth_server_request(t_authresponse *authresponse, const char *request_type, cons
 			return(AUTH_ERROR);
 		}
 	}
-	else {
-		return(AUTH_ERROR);
-	}
 
-	/* XXX Never reached because of the above if()/else pair. */
 	return(AUTH_ERROR);
 }
 
