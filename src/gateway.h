6512b802b87d06f7586f0f5657e2e1f9d2a64da4
Michael Haas <haas@computerlinguist.org>
2015-03-25 09:35:18 +0100

    Indent gateway{c,h}

--- src/gateway.h
+++ src/gateway.h
@@ -26,8 +26,8 @@
 
 #ifndef _GATEWAY_H_
 #define _GATEWAY_H_
- 
+
 /** @brief exits cleanly and clear the firewall rules. */
 void termination_handler(int s);
 
-#endif /* _GATEWAY_H_ */
+#endif                          /* _GATEWAY_H_ */
