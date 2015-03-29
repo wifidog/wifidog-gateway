ca2771ff3a44198ee6a255f32278f381974dcc64
Alexandre Carmel-Veilleux <acv@miniguru.ca>
2015-03-29 13:10:42 -0400

    Vim modelines

--- src/fw_iptables.c
+++ src/fw_iptables.c
@@ -1,3 +1,4 @@
+/* vim: set et ts=4 sts=4 sw=4 : */
 /********************************************************************\
  * This program is free software; you can redistribute it and/or    *
  * modify it under the terms of the GNU General Public License as   *
