d8099977eaf80aa2c8fba72f87a2302ce9877305
Lilian Bonnafoux <florida@assf.fr>
2015-03-30 19:06:32 +0200

    Revert "rename User-Agent "
    This reverts commit b685ad0d60927c766fbe0c57ab5dc099699de31a.

--- src/ping_thread.c
+++ src/ping_thread.c
@@ -158,7 +158,7 @@ ping(void)
      */
     snprintf(request, sizeof(request) - 1,
              "GET %s%sgw_id=%s&sys_uptime=%lu&sys_memfree=%u&sys_load=%.2f&wifidog_uptime=%lu HTTP/1.0\r\n"
-             "User-Agent: WiFiDog-ffw %s\r\n"
+             "User-Agent: WiFiDog %s\r\n"
              "Host: %s\r\n"
              "\r\n",
              auth_server->authserv_path,
