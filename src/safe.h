21876e800e9157e4de7c2e95fd855f2ade4e65f8
Alexandre Carmel-Veilleux <acv@miniguru.ca>
2015-04-01 22:18:25 -0400

    Decouple safe.c from knowing anything about libhttpd

--- src/safe.h
+++ src/safe.h
@@ -32,28 +32,25 @@
 #include <sys/types.h>          /* For fork */
 #include <unistd.h>             /* For fork */
 
-/** @brief Safe version of malloc
- */
+/** Register an fd for auto-cleanup on fork() */
+void register_fd_cleanup_on_fork(const int);
+
+/** @brief Safe version of malloc */
 void *safe_malloc(size_t);
 
 /** @brief Safe version of realloc */
 void *safe_realloc(void *, size_t);
 
-/* @brief Safe version of strdup
- */
+/* @brief Safe version of strdup */
 char *safe_strdup(const char *);
 
-/* @brief Safe version of asprintf
- */
+/* @brief Safe version of asprintf */
 int safe_asprintf(char **, const char *, ...);
 
-/* @brief Safe version of vasprintf
- */
+/* @brief Safe version of vasprintf */
 int safe_vasprintf(char **, const char *, va_list);
 
-/* @brief Safe version of fork
- */
-
+/* @brief Safe version of fork */
 pid_t safe_fork(void);
 
 #endif                          /* _SAFE_H_ */
