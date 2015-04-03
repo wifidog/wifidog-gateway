6302dda6f06fd357b10c59f6168bf608e62253ae
Michael Haas <haas@computerlinguist.org>
2015-04-03 18:49:24 +0200

    Capabilities: error handling, comment

--- src/capabilities.c
+++ src/capabilities.c
@@ -4,10 +4,14 @@
 
 #ifdef USE_LIBCAP
 
+#include <errno.h>
+
 #include <syslog.h>
 #include <sys/capability.h>
 #include <sys/prctl.h>
 #include <unistd.h>
+/* For strerror */
+#include <string.h>
 /* For exit */
 #include <stdlib.h>
 /* For getpwnam */
@@ -35,7 +39,7 @@
  * Any error is considered fatal and exit() is called.
  *
  * @param user Non-privileged user
- * @param Non-privileged group
+ * @param group Non-privileged group
  */
 void
 drop_privileges(const char *user, const char *group)
@@ -47,6 +51,7 @@ drop_privileges(const char *user, const char *group)
      * CAP_NET_ADMIN is not used directly by iptables which
      * is called by Wifidog
      */
+    const int num_caps = 2;
     cap_value_t cap_values[] = { CAP_NET_RAW, CAP_NET_ADMIN };
     cap_t caps;
 
@@ -54,7 +59,7 @@ drop_privileges(const char *user, const char *group)
     debug(LOG_DEBUG, "Current capabilities: %s", cap_to_text(caps, NULL));
     /* Clear all caps and then set the caps we desire */
     cap_clear(caps);
-    cap_set_flag(caps, CAP_PERMITTED, 2, cap_values, CAP_SET);
+    cap_set_flag(caps, CAP_PERMITTED, num_caps, cap_values, CAP_SET);
     ret = cap_set_proc(caps);
     if (ret == -1) {
         debug(LOG_ERR, "Could not set capabilities!");
@@ -74,30 +79,34 @@ drop_privileges(const char *user, const char *group)
     debug(LOG_DEBUG, "Switching to group %s", group);
     /* don't free grp, see getpwnam() for details */
     struct group *grp = getgrnam(group);
-    if (grp) {
-        gid_t gid = grp->gr_gid;
-        // TODO: check retval
-        setegid(gid);
-    } else {
-        debug(LOG_ERR, "Failed to look up GID for group %s", group);
+    if (NULL == grp) {
+        debug(LOG_ERR, "Failed to look up GID for group %s: %s", group, strerror(errno));
+        exit(1);
+    }
+    gid_t gid = grp->gr_gid;
+    ret = setegid(gid);
+    if (ret != 0) {
+        debug(LOG_ERR, "Failed to setegid() for group %s: %s", group, strerror(errno));
         exit(1);
     }
     debug(LOG_DEBUG, "Switching to user %s", user);
-    /* don't pwd, see getpwnam() for details */
+    /* don't free pwd, see getpwnam() for details */
     struct passwd *pwd = getpwnam(user);
-    if (pwd) {
-        uid_t uid = pwd->pw_uid;
-        // TODO: check retval
-        seteuid(uid);
-    } else {
+    if (NULL == pwd) {
         debug(LOG_ERR, "Failed to look up UID for user %s", user);
+        exit(1);
+    }
+    uid_t uid = pwd->pw_uid;
+    ret = seteuid(uid);
+    if (ret != 0) {
+        debug(LOG_ERR, "Failed to seteuid() for user %s: %s", user, strerror(errno));
         exit(1);
     }
     caps = cap_get_proc();
     debug(LOG_DEBUG, "Current capabilities: %s", cap_to_text(caps, NULL));
     debug(LOG_DEBUG, "Regaining capabilities.");
     /* Re-gain privileges */
-    cap_set_flag(caps, CAP_EFFECTIVE, 1, cap_values, CAP_SET);
+    cap_set_flag(caps, CAP_EFFECTIVE, num_caps, cap_values, CAP_SET);
     /* Note that we keep CAP_INHERITABLE empty. In theory, CAP_INHERITABLE
      * would be useful to execve iptables as non-root. In practice, Wifidog
      * often runs on embedded systems (OpenWrt) where the required file-based
@@ -110,13 +119,19 @@ drop_privileges(const char *user, const char *group)
      * capabilities on the executable. Alas, it's not relevant here.
      *
      * This is also the main reason why we only seteuid() instead of setuid():
-     * When an executable is called as root (real UID == 0), the INHERITED
-     * and PERMITTED file capabilities are implicitly marked as enabled.
+     * When an executable is called as root (real UID == 0), the INHERITABLE
+     * and PERMITTED file capability sets are implicitly marked as enabled.
      *
-     * TODO: what about EFFECTIVE? What is set-user-id-root in this context?
-     * Iptables probably works because of (F(permitted) & cap_bset)
-     * So, what is the bounding set after we're done?
+     * TODO:
+     * It is not clear to me why iptables has the necessary capabilities in its
+     * EFFECTIVE set. This should only happen for SUID0 executables. I also do
+     * not understand why iptables receives the necessary capabilities at all:
      *
+     *  P'(permitted) = (P(inheritable) & F(inheritable)) |
+     *                      (F(permitted) & cap_bset)
+     *
+     * Since P(inheritable) is empty, P'(permitted) should be empty as well
+     * unless cap_bset is automatically populated.
      */
     ret = cap_set_proc(caps);
     if (ret == -1) {
@@ -124,7 +139,7 @@ drop_privileges(const char *user, const char *group)
         exit(1);
     }
     caps = cap_get_proc();
-    debug(LOG_DEBUG, "Regained: %s", cap_to_text(caps, NULL));
+    debug(LOG_INFO, "Final capabilities: %s", cap_to_text(caps, NULL));
     cap_free(caps);
 }
 
