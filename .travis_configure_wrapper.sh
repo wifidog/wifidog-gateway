f5070787a6fd1fc871cbe9088a5d7b3b72acd39b
Michael Haas <haas@computerlinguist.org>
2015-04-08 11:05:54 +0200

    Refactor travis_configure_wrapper
    Also fix build and install of libattr

--- .travis_configure_wrapper.sh
+++ .travis_configure_wrapper.sh
@@ -1,26 +1,44 @@
 #!/usr/bin/env bash
 
-if [[ -z "$BUILD_TYPE" ]]; then
-    echo "BUILD_TYPE not set. Bye."
-    exit 1
-fi
 
-if [[ "$BUILD_TYPE" == "normal" ]]; then
-
-    echo "Running Wifidog configure"
-    ./configure $@
-
-elif [[ "$BUILD_TYPE" == "full" ]]; then
-    if [[ -z "$CYASSL" ]]; then
-        echo "CYASSL not set."
+function main {
+    if [[ -z "$BUILD_TYPE" ]]; then
+        echo "BUILD_TYPE not set. Bye."
         exit 1
     fi
-    CUR=`pwd`
-    mkdir -p dependencies-src || true
-    mkdir -p dependencies-installed || true
+
+    if [[ "$BUILD_TYPE" == "normal" ]]; then
+
+        echo "Running Wifidog configure"
+        ./configure $@
+
+    elif [[ "$BUILD_TYPE" == "full" ]]; then
+        if [[ -z "$CYASSL" || -z "$LIBCAP" || -z "$LIBATTR" ]]; then
+            echo "Make sure that CYASSL, LIBCAP and LIBATTR are set."
+            exit 1
+        fi
+        mkdir -p dependencies-src || true
+        mkdir -p dependencies-installed || true
+        build_cyassl
+        build_libattr
+        build_libcap
+
+        echo "Running Wifidog configure"
+        export CFLAGS="${CFLAGS} -I${CUR}/dependencies-installed/include/"
+        export LDFLAGS="${CFLAGS} -L${CUR}/dependencies-installed/lib/"
+        ./configure --enable-cyassl --enable-libcap $@
+    else
+        echo "Unknow BUILD_TYPE $BUILD_TYPE"
+        exit 1
+    fi
+
+}
+
+function build_cyassl {
     # TODO: changing $CYASSL version number will not invalidate this check
     # Need to remove full cache in travis interface if we want to upgrade
     # CyaSSL
+    CUR=`pwd`
     if [[ ! -f dependencies-installed/include/cyassl/ssl.h ]]; then
         echo "Cached CyaSSL install not found. Installing."
         cd dependencies-src
@@ -50,7 +68,11 @@ elif [[ "$BUILD_TYPE" == "full" ]]; then
     else
         echo "Cached CyaSSL install found."
     fi
-    if [[ ! -f dependencies-installed/include/sys/capability.h ]]; then
+}
+
+function build_libcap {
+    CUR=`pwd`
+    if [[ ! -f dependencies-installed/usr/include/sys/capability.h ]]; then
         echo "Cached libcap not found. Installing."
         cd dependencies-src
         if [[ -f libcap-${LIBCAP}/Makefile ]]; then
@@ -67,11 +89,16 @@ elif [[ "$BUILD_TYPE" == "full" ]]; then
         echo "Running libcap make"
         make
         echo "Running libcap make install"
-        make install DESTDIR="$CUR"/dependencies-installed/
-        cd $CUR
+        make install DESTDIR="$CUR"/dependencies-installed/ RAISE_SETFCAP=no
+        cd "$CUR"
     else
         echo "Cached libcap install found."
     fi
+
+}
+
+function build_libattr {
+    CUR=`pwd`
     if [[ ! -f dependencies-installed/include/attr/libattr.h ]]; then
         echo "Cached libattr not found. Installing."
         cd dependencies-src
@@ -89,40 +116,11 @@ elif [[ "$BUILD_TYPE" == "full" ]]; then
         echo "Running attr configure"
         ./configure --prefix="$CUR"/dependencies-installed/
         echo "Running attr make install"
-        make install
+        make install install-dev install-lib
         cd $CUR
     else
         echo "Cached attr install found."
     fi
-    if [[ ! -f dependencies-installed/include/sys/capability.h ]]; then
-        echo "Cached libcap not found. Installing."
-        cd dependencies-src
-        if [[ -f libcap-${LIBCAP}/Makefile ]]; then
-            echo "Found cached libcap package"
-        else
-            echo "No cache, downloading libcap"
-            wget https://www.kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-${LIBCAP}.tar.gz \
-                -O libcap-${LIBCAP}.tar.gz
-            tar -xvzf libcap-${LIBCAP}.tar.gz
-        fi
-        cd libcap-${LIBCAP}
-        echo "Content of libcap-${LIBCAP}"
-        ls
-        echo "Running libcap make"
-        make
-        echo "Running libcap make install"
-        make install DESTDIR="$CUR"/dependencies-installed/
-        cd $CUR
-    else
-        echo "Cached libcap install found."
-    fi
+}
 
-
-    echo "Running Wifidog configure"
-    export CFLAGS="${CFLAGS} -I${CUR}/dependencies-installed/include/"
-    export LDFLAGS="${CFLAGS} -L${CUR}/dependencies-installed/lib/"
-    ./configure --enable-cyassl --enable-libcap $@
-else
-    echo "Unknow BUILD_TYPE $BUILD_TYPE"
-    exit 1
-fi
+main
