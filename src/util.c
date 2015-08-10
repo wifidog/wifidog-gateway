acb74c5630cc2e7169bf563e1a41a01585fa3c1a
tymon.huang <banglang.huang@foxmail.com>
2015-08-10 09:53:36 +0800

    fix control flow error
    Signed-off-by: tymon.huang <banglang.huang@foxmail.com>

--- src/util.c
+++ src/util.c
@@ -405,10 +405,8 @@ save_pid_file(const char *pf)
             int ret = fclose(f);
             if (ret == EOF) /* check the return value of fclose */
                 debug(LOG_ERR, "fclose() on file %s was failed (%s)", pf, strerror(errno));
-        }
-
-        /* fopen return NULL, open file failed */
-        debug(LOG_ERR, "fopen() on flie %s was failed (%s)", pf, strerror(errno));
+        } else /* fopen return NULL, open file failed */
+            debug(LOG_ERR, "fopen() on flie %s was failed (%s)", pf, strerror(errno));
     }
 
     return;
